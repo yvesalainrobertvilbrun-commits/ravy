301b36ca1fd9ce994d7df9a263f850686cc546bd
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-06 14:35:13 -0400

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,23 @@
-const CACHE_NAME = "ravy-cache-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const cacheName = 'ravy-cache-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Instalar service worker
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-// Activar service worker
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Interceptar peticiones
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
