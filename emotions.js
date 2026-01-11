f787278d04f12926392006907658027fa0b4c4e5
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-10 21:58:55 -0400

    Update emotions.js

--- emotions.js
+++ emotions.js
@@ -1,9 +1,8 @@
-export function getEmotion(texto){
-  texto = texto.toLowerCase();
+// ================= EMOCIONES =================
 
-  if(texto.includes("triste") || texto.includes("mal")) return "triste";
-  if(texto.includes("feliz") || texto.includes("bien")) return "feliz";
-  if(texto.includes("miedo")) return "miedo";
-
+export function detectarEmocion(texto) {
+  if (texto.includes("triste") || texto.includes("mal")) return "triste";
+  if (texto.includes("feliz") || texto.includes("bien")) return "feliz";
+  if (texto.includes("miedo")) return "miedo";
   return "neutral";
 }
