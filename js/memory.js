911b713f80c61699616e0673a49460d092f892e0
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-24 08:55:31 -0400

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -2,7 +2,7 @@ export let memory = [];
 
 export function saveMessage(msg) {
   memory.push(msg);
-  if(memory.length > 50) memory.shift(); // solo últimos 50 mensajes
+  if(memory.length > 50) memory.shift();
 }
 
 export function getMemory() {
