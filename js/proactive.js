c71415f02a524d5e87ed4e81f1ddf469c69c6a7d
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-23 22:57:23 -0400

    Update proactive.js

--- js/proactive.js
+++ js/proactive.js
@@ -1,1 +1,31 @@
+// js/proactive.js
 
+let proactiveTimer = null;
+
+const proactiveMessages = [
+  "Sigo aquí contigo.",
+  "¿En qué estabas pensando?",
+  "Si quieres, podemos seguir.",
+  "A veces el silencio también habla.",
+  "No tienes que escribir perfecto."
+];
+
+function randomMessage() {
+  return proactiveMessages[
+    Math.floor(Math.random() * proactiveMessages.length)
+  ];
+}
+
+export function startProactive(callback, delay = 15000) {
+  stopProactive();
+  proactiveTimer = setTimeout(() => {
+    callback(randomMessage());
+  }, delay);
+}
+
+export function stopProactive() {
+  if (proactiveTimer) {
+    clearTimeout(proactiveTimer);
+    proactiveTimer = null;
+  }
+}
