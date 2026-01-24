95ef69aa5b88e411a7ddf6e1db230dd0eed18608
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-24 08:49:21 -0400

    Update ravy.js

--- js/ravy.js
+++ js/ravy.js
@@ -1,9 +1,3 @@
-import { ravyRespond } from './ravy-core.js';
-
-const chatBox = document.getElementById("ravy-chat");
-const input = document.getElementById("ravy-input");
-const sendBtn = document.getElementById("ravy-send");
-
 function addMessage(content, sender="user") {
   const div = document.createElement("div");
   div.className = sender;
@@ -18,17 +12,3 @@ function addMessage(content, sender="user") {
   chatBox.appendChild(div);
   chatBox.scrollTop = chatBox.scrollHeight;
 }
-
-function sendMessage() {
-  const text = input.value.trim();
-  if(!text) return;
-
-  addMessage(text, "user");
-  ravyRespond(text, res => addMessage(res, "ravy"));
-  input.value = "";
-}
-
-sendBtn.addEventListener("click", sendMessage);
-input.addEventListener("keypress", e => {
-  if(e.key === "Enter") sendMessage();
-});
