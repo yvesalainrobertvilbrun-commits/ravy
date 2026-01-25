2195ec2bfec0066ab460f7022f3f3a2309d05313
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-24 21:53:05 -0400

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,12 @@
 export function save(key, value) {
-  localStorage.setItem(key, value);
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
 export function load(key) {
-  return localStorage.getItem(key);
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+}
+
+export function resetMemory() {
+  localStorage.clear();
 }
