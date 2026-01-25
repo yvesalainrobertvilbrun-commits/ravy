41f55bdd6c620622790c106a30f3d4b62ddd3d05
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-24 20:52:47 -0400

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,34 +1,7 @@
-// ===== RESPUESTAS APRENDIDAS =====
-export function learnResponse(question, answer) {
-  const memory = JSON.parse(localStorage.getItem("ravy_memory") || "{}");
-  memory[question] = answer;
-  localStorage.setItem("ravy_memory", JSON.stringify(memory));
+export function save(key, value) {
+  localStorage.setItem(key, value);
 }
 
-export function getLearnedResponse(question) {
-  const memory = JSON.parse(localStorage.getItem("ravy_memory") || "{}");
-  return memory[question];
-}
-
-// ===== NOMBRE DEL USUARIO =====
-export function saveUserName(name) {
-  localStorage.setItem("ravy_user_name", name);
-}
-
-export function getUserName() {
-  return localStorage.getItem("ravy_user_name");
-}
-
-// ===== CREADOR =====
-export function getCreatorName() {
-  return "Yves";
-}
-
-// ===== CIUDAD DEL USUARIO =====
-export function saveUserCity(city) {
-  localStorage.setItem("ravy_city", city);
-}
-
-export function getUserCity() {
-  return localStorage.getItem("ravy_city");
+export function load(key) {
+  return localStorage.getItem(key);
 }
